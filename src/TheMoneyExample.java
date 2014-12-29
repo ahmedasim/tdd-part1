710d54a3c7db14cdb240ec4612bc32307de3e5fd
ahmedasim <sendtoasimOff@gmail.com>
2014-12-29 16:24:02 +0500

    Make amount private( Test )
    With these changes in TheMoneyExample. it clearly shows that Dollar is only class that is using amount field, so it should be private.

--- src/TheMoneyExample.java
+++ src/TheMoneyExample.java
@@ -9,15 +9,15 @@ public class TheMoneyExample {
 	@Test
 	public void testMultiplication() {
 		Dollar ten = new Dollar(10);
-		Dollar product = ten.times(3);
-		assertEquals(30, product.amount);
-		product = ten.times(2);
-		assertEquals(20, product.amount);
+		//Dollar product = ten.times(3);
+		assertEquals(new Dollar(30), ten.times(3));
+		//product = ten.times(2);
+		assertEquals(new Dollar(20), ten.times(2));
 	}
 	@Test 
 	public void testEquality( ){
 		assertTrue(new Dollar(30).equals(new Dollar(30)));
-		assertFalse(new Dollar(30).equals(new Dollar(30)));
+		assertFalse(new Dollar(20).equals(new Dollar(30)));
 	}
 
 }

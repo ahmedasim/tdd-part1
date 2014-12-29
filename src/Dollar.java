ceca63a4e539347aa9361669f64099bec5222940
ahmedasim <sendtoasimOff@gmail.com>
2014-12-29 16:08:39 +0500

    Equlity funtionlity
    Added equals method to compare the dollar objects. This method is applicable for Dollar objects, but not for null or other objects.

--- src/Dollar.java
+++ src/Dollar.java
@@ -11,5 +11,9 @@ public class Dollar {
 	public Dollar times(int multiplier){
 		return new Dollar(this.amount * multiplier);
 	}
+	public boolean equals( Object object){
+		Dollar dollar = (Dollar) object;
+		return dollar.amount == this.amount;
+	}
 
 }

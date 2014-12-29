5920d566b157cb342907cdeea7440525115a8806
ahmedasim <sendtoasimOff@gmail.com>
2014-12-29 15:57:47 +0500

    Side effect Dollar class
    Dollar object ten amount will be changed after calling times method and when times method call again it will cause an error.
    Times method should return new dollar object so ten dollar object amount won't change.

--- src/Dollar.java
+++ src/Dollar.java
@@ -8,8 +8,8 @@ public class Dollar {
 		this.amount = amount;
 	}
 	
-	public void times(int multiplier){
-		this.amount *=  multiplier;
+	public Dollar times(int multiplier){
+		return new Dollar(this.amount * multiplier);
 	}
 
 }

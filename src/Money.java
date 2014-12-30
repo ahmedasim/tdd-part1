0a2158f9affe8a7790a251cf35e3471891928f2e
ahmedasim <sendtoasimOff@gmail.com>
2014-12-30 11:52:48 +0500

    Added money class for equality between dollar and franc
    Added money class for equality between dollar and franc. Added amount variable protected so child can see it. Added equal method for both Franc and Dollar

---
+++ src/Money.java
@@ -0,0 +1,10 @@
+package TheMoneyExample;
+
+public class Money {
+	protected int amount;
+	
+	public boolean equals(Object object) {
+		Money money= (Money) object;
+		return amount == money.amount;
+	}
+}

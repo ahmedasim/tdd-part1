a42d3268ddc421548136d4e45f19608d7597de43
ahmedasim <sendtoasimOff@gmail.com>
2014-12-31 10:27:39 +0500

    Added currency name
    Added private string variable for currency name.

--- src/Franc.java
+++ src/Franc.java
@@ -2,13 +2,17 @@ package TheMoneyExample;
 
 public class Franc extends Money {
 	//private int amount;
-	
+	private String currency;
 	Franc(int amount){
 		this.amount = amount;
+		currency = "CHF";
 	}
 	
 	public Franc times( int multiplier){
 		return new Franc(this.amount * multiplier);
+	}
+	String currency() {
+		return "CHF";
 	}
 	
 }

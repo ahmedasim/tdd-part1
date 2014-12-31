e322877bc8e68d5c9a672e45ceecfb0a38a54345
ahmedasim <sendtoasimOff@gmail.com>
2014-12-31 10:25:27 +0500

    Added test for currency
    Added test for currency name. Used a private string variabled in Dollar and Franc class both

--- src/TheMoneyExample.java
+++ src/TheMoneyExample.java
@@ -8,29 +8,31 @@ public class TheMoneyExample {
 
 	@Test
 	public void testMultiplication() {
-		Dollar ten = new Dollar(10);
-		//Dollar product = ten.times(3);
-		assertEquals(new Dollar(30), ten.times(3));
-		//product = ten.times(2);
-		assertEquals(new Dollar(20), ten.times(2));
+		Money five = Money.dollar(5);
+		assertEquals(new Dollar(10), five.times(2));
+		assertEquals(new Dollar(15), five.times(3));
 	}
 	@Test 
 	public void testEquality( ){
-		assertTrue(new Dollar(10).equals(new Dollar(10)));
-		assertFalse(new Dollar(50).equals(new Dollar(60)));
+		assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+		assertFalse(Money.dollar(5).equals(Money.dollar(6)));
 		assertTrue(new Franc(5).equals(new Franc(5)));
 		assertFalse(new Franc(5).equals(new Franc(6)));
+		assertFalse(new Franc(5).equals(Money.dollar(5)));
 	}
 	
 	@Test
 	public void testFranMultiplication() {
-		Franc five = new Franc(5);
-		//Dollar product = ten.times(3);
-		assertEquals(new Franc(15), five.times(3));
-		//product = ten.times(2);
-		assertEquals(new Franc(10), five.times(2));
+		Money five = Money.franc(5);
+		assertEquals(Money.franc(10), five.times(2));
+		assertEquals(Money.franc(15), five.times(3));
 	}
 	
+	@Test 
+	public void testCurrency() {
+		assertEquals("USD", Money.dollar(1).currency());
+		assertEquals("CHF", Money.franc(1).currency());
+	}
 	
 	
 }

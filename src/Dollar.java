fc630411e26de733ffd1dd01816bfe8e3dcfb8f7
ahmedasim <sendtoasimOff@gmail.com>
2014-12-31 10:27:03 +0500

    Added currency name
    Added private string variable for currency name.

--- src/Dollar.java
+++ src/Dollar.java
@@ -4,12 +4,17 @@ public class Dollar extends Money {
 	
 	//private int amount;
 	
-	Dollar(int amount){
+	private String currency;
+	Dollar(int amount) {
 		this.amount = amount;
+		currency = "USD";
 	}
 	
 	public Dollar times(int multiplier){
 		return new Dollar(this.amount * multiplier);
 	}
-
+	
+	String currency() {
+		return "USD";
+	}
 }

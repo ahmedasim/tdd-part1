07ce7671922dce1f892b050b3217009bd7e8daf0
ahmedasim <sendtoasimOff@gmail.com>
2015-01-03 17:24:23 +0500

    Chapter 16

--- src/Sum.java
+++ src/Sum.java
@@ -13,6 +13,10 @@ public class Sum implements Expression{
 		return new Money(amount, to);
 	}
 	public Expression plus(Expression addend) {
-		return null;
+		return new Sum(this, addend);
+	}
+	
+	public Expression times(int multiplier) {
+		return new Sum(augend.times(multiplier),addend.times(multiplier));
 	}
 }

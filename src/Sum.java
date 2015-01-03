db2f40e18402a7262ab7ba8aec900913131e5936
ahmedasim <sendtoasimOff@gmail.com>
2015-01-03 17:18:52 +0500

    Chapter 15

--- src/Sum.java
+++ src/Sum.java
@@ -1,15 +1,18 @@
 package TheMoneyExample;
 
 public class Sum implements Expression{
-	Money augend;
-	Money addend;
-	Sum(Money augend, Money addend) {
-		this.augend = augend;
-		this.addend = addend;
+	Expression augend;
+	Expression addend;
+	Sum(Expression augend, Expression addend) {
+		this.augend= augend;
+		this.addend= addend;
 	}
-	
 	public Money reduce(Bank bank, String to) {
-		int amount= augend.amount + addend.amount;
+		int amount= augend.reduce(bank, to).amount
+		+ addend.reduce(bank, to).amount;
 		return new Money(amount, to);
+	}
+	public Expression plus(Expression addend) {
+		return null;
 	}
 }

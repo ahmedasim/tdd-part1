464236cff895a68dc1a7929a360604efeb0ec21d
ahmedasim <sendtoasimOff@gmail.com>
2015-01-03 17:12:48 +0500

    Chapter 14

--- src/TheMoneyExample.java
+++ src/TheMoneyExample.java
@@ -35,12 +35,43 @@ public class TheMoneyExample {
 	public void testDifferentClassEquality() {
 		assertTrue(new Money(10, "CHF").equals(new Franc(10, "CHF")));
 	}
-	
+	@Test
 	public void testSimpleAddition() {
 		Money five= Money.dollar(5);
 		Expression sum= five.plus(five);
 		Bank bank= new Bank();
 		Money reduced= bank.reduce(sum, "USD");
 		assertEquals(Money.dollar(10), reduced);
+	}
+	@Test
+	public void testPlusReturnsSum() {
+		Money five= Money.dollar(5);
+		Expression result= five.plus(five);
+		Sum sum= (Sum) result;
+		assertEquals(five, sum.augend);
+		assertEquals(five, sum.addend);
+	}
+	@Test
+	public void testReduceSum() {
+		Expression sum= new Sum(Money.dollar(3), Money.dollar(4));
+		Bank bank= new Bank();
+		Money result= bank.reduce(sum, "USD");
+		assertEquals(Money.dollar(7), result);
+	}
+	@Test
+	public void testReduceMoney() {
+		Bank bank= new Bank();
+		Money result= bank.reduce(Money.dollar(1), "USD");
+		assertEquals(Money.dollar(1), result);
+	}
+	
+	public void testReduceMoneyDifferentCurrency() {
+		Bank bank= new Bank();
+		bank.addRate("CHF", "USD", 2);
+		Money result= bank.reduce(Money.franc(2), "USD");
+		assertEquals(Money.dollar(1), result);
+	}
+	public void testIdentityRate() {
+		assertEquals(1, new Bank().rate("USD", "USD"));
 	}
 }
